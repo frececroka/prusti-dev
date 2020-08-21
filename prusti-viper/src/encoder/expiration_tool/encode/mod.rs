9b318276880fe74ef273609bc319e27f92ef4ded
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-08-21 12:41:38 +0200

    Add shortcuts for tcx, def_id, local_def_id, and called MIR.

--- prusti-viper/src/encoder/expiration_tool/encode/mod.rs
+++ prusti-viper/src/encoder/expiration_tool/encode/mod.rs
@@ -1,10 +1,15 @@
+use std::cell::Ref;
+
 use binding::Binding;
 use binding::LiftBindings;
 use prusti_common::vir;
+use rustc_hir::def_id::DefId;
+use rustc_hir::def_id::LocalDefId;
 use rustc_middle::mir;
+use rustc_middle::ty;
 
-use crate::encoder::procedure_encoder::ProcedureEncoder;
 use crate::encoder::borrows::ProcedureContract;
+use crate::encoder::procedure_encoder::ProcedureEncoder;
 
 mod expression;
 mod package;
@@ -59,5 +64,25 @@ impl<'a, 'p, 'v: 'p, 'tcx: 'v> ExpirationToolEncoder<'a, 'p, 'v, 'tcx> {
             encoded_args, encoded_return,
             pre_label, post_label
         }
+    }
+
+    fn tcx(&self) -> ty::TyCtxt<'tcx> {
+        self.procedure_encoder.procedure.get_tcx()
+    }
+
+    fn def_id(&self) -> DefId {
+        self.contract.def_id
+    }
+
+    fn local_def_id(&self) -> LocalDefId {
+        self.def_id().expect_local()
+    }
+
+    /// The MIR of the function that this expiration tool belongs to.
+    /// TODO: The name `called_mir` may be a bit bad.
+    fn called_mir(&self) -> Ref<mir::Body<'tcx>> {
+        let local_def_id = self.local_def_id();
+        let (mir, _) = self.tcx().mir_validated(ty::WithOptConstParam::unknown(local_def_id));
+        mir.borrow()
     }
 }
