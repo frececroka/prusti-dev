7d12d5815b3bee46c11476c4cb21a66639d244a2
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-08-28 16:11:25 +0200

    Run caller test cases on CI.

---
+++ prusti-tests/tests_old/parse/ui/pledges-callers.rs
@@ -0,0 +1,34 @@
+#![feature(register_tool)]
+#![register_tool(prusti)]
+
+use prusti_contracts::*;
+
+struct Point { x: u32, y: u32 }
+
+fn f0(p: &mut Point) -> &mut u32 {
+    p.y = 0;
+    &mut p.x
+}
+
+fn f0_caller() {
+    let mut p = Point { x: 0, y: 0 };
+    let px = f0(&mut p);
+    *px += 1;
+}
+
+#[pledge(after_unblocked(p.x) == before_expiry(*result))]
+#[pledge(after_unblocked(p.y) == 0)]
+#[ensures(*result == old(p.x))]
+fn f1(p: &mut Point) -> &mut u32 {
+    p.y = 0;
+    &mut p.x
+}
+
+fn f1_caller() {
+    let mut p = Point { x: 0, y: 0 };
+    let px = f1(&mut p);
+    *px += 1;
+    assert!(p.x == 1);
+}
+
+fn main() {}
