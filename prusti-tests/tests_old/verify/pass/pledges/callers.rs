f4f427b46b46891dc716660ff63c40dd06e4ad5c
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-08-28 16:09:17 +0200

    Add simple caller test cases.

--- prusti-tests/tests_old/verify/pass/pledges/callers.rs
+++ prusti-tests/tests_old/verify/pass/pledges/callers.rs
@@ -5,16 +5,28 @@ use prusti_contracts::*;
 
 struct Point { x: u32, y: u32 }
 
-#[pledge(after_unblocked(p.x) == before_expiry(*result))]
-#[pledge(after_unblocked(p.y) == 0)]
 fn f0(p: &mut Point) -> &mut u32 {
     p.y = 0;
     &mut p.x
 }
 
-fn f1() {
+fn f0_caller() {
     let mut p = Point { x: 0, y: 0 };
     let px = f0(&mut p);
+    *px += 1;
+}
+
+#[pledge(after_unblocked(p.x) == before_expiry(*result))]
+#[pledge(after_unblocked(p.y) == 0)]
+#[ensures(*result == old(p.x))]
+fn f1(p: &mut Point) -> &mut u32 {
+    p.y = 0;
+    &mut p.x
+}
+
+fn f1_caller() {
+    let mut p = Point { x: 0, y: 0 };
+    let px = f1(&mut p);
     *px += 1;
     assert!(p.x == 1);
 }
