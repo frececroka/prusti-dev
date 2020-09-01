12308f90d8ee7a04debd28ccb805dfea4e956159
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-09-01 13:19:24 +0200

    More tests.

--- prusti-tests/tests_old/parse/ui/pledges-callers.rs
+++ prusti-tests/tests_old/parse/ui/pledges-callers.rs
@@ -44,11 +44,50 @@ fn f3<'p, 'q>(p: &'p mut Point, q: &'q mut Point) -> (&'p mut u32, &'q mut u32) 
 }
 
 fn f3_caller() {
-    let mut p = Point { x: 10, y: 11 };
-    let mut q = Point { x: 20, y: 21 };
+    let mut p = Point { x: 10, y: 20 };
+    let mut q = Point { x: 30, y: 40 };
     let (px, qx) = f3(&mut p, &mut q);
     *px += 1;
     *qx += 2;
+}
+
+#[pledge(after_unblocked(p.x) == before_expiry(*result.0))]
+#[ensures(*result.0 == old(p.x))]
+fn f4<'p, 'q>(p: &'p mut Point, q: &'q mut Point) -> (&'p mut u32, &'q mut u32) {
+    (&mut p.x, &mut q.x)
+}
+
+fn f4_caller() {
+    let mut p = Point { x: 10, y: 20 };
+    let mut q = Point { x: 30, y: 40 };
+    let (px, _) = f4(&mut p, &mut q);
+    *px += 1;
+    // TODO: This does not verify due to the way output references of function calls are handled.
+    //  First of all, when the magic wand for the call to f4 is applied, the pledges inside it
+    //  don't use the value of px returned by the function call, but instead the current value of
+    //  px. This is necessary because an assignment like "*px = *x" does not change the memory
+    //  location that px is pointing to, but in Viper this is encoded as "px.val_ref = x.val_ref",
+    //  which does change the memory location px is pointing to. Because we'd like to assert after
+    //  the assignment "*px = *x" that all the places aliased by px now also hold the value *x, we
+    //  must use the current value of px.val_ref in the magic wand, not the one from right after
+    //  the call.
+    //  //
+    //  Now this would work fine as long as Rust doesn't move references around. Consider what is
+    //  happening when we call f4 like this:
+    //      let (px, _) = f4(...);
+    //  The MIR models this as two statements (slightly simplified):
+    //      _1 = f4(...);
+    //      _2 = move _1.0;
+    //  In Viper, this is encoded like this:
+    //      _1 = f4(...);
+    //      _2.val_ref = _1.tuple_0.val_ref;
+    //  And later on, when we overwrite *_2, Viper generates the following statement:
+    //      _2.val_ref = ...;
+    //  Finally, we expire _1.0 and would like to use the current value in the pledges. Instead of
+    //  using the value of _1.tuple_0.val_ref from right after the function call, we use the
+    //  current value instead. The problem is that because _1.0 was moved into _2 earlier, we
+    //  actually have to use the current value of _2, not the current value of _1.0.
+    // assert!(p.x == 11);
 }
 
 fn main() {}
